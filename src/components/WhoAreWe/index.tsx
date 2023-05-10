c7288c58b952a1c9d64bfa49765e3817db4de6f6
Lucas Sagas <lucassaagas@gmail.com>
2023-05-10 14:42:22 -0300

    fix porteguese error

--- src/components/WhoAreWe/index.tsx
+++ src/components/WhoAreWe/index.tsx
@@ -7,15 +7,14 @@ export function WhoAreWe(): JSX.Element {
     <Container>
       <h1>Quem Somos</h1>
       <p>
-        A Three Soft é uma empresa de desenvolvimento de sistemas focada em construir ferramentas de
-        inteligência para auxiliar empreendedores na tomada de decisões que possam fazer a diferença
-        nas negociaçõe se e gerenciamento de demandas.
+        A Three Soft é uma empresa de desenvolvimento de sistema, focado em construir ferramentas
+        para auxiliar empreendedores em seu gerenciamento.
       </p>
 
       <div>
         <img
           src={laptopImg}
-          alt=' imagem de um jovem segurando um celular em frente ao notebook com o sistema Three soft aberto'
+          alt='imagem de um jovem segurando um celular em frente ao notebook com o sistema Three soft aberto'
         />
         <p>
           Prezamos por qualidade e velocidade, cada tela do nosso sistema é pensada em otimização
@@ -25,8 +24,8 @@ export function WhoAreWe(): JSX.Element {
 
       <section>
         <p>
-          A Three Software é uma empresa que fica localizada em Biguaçu/SC na Grande Florianópolis,
-          onde se encontra os principais centros de tecnologia de software, respiramos as melhores
+          A Three Soft é uma empresa que fica localizada em Biguaçu/SC na Grande Florianópolis, onde
+          se encontra os principais centros de tecnologia de software, respiramos as melhores
           tendências e soluções do mercado para implementação.
         </p>
         <img src={mapAbout} alt='foto de uma mapa com as CTO marcadas no mapa' />
